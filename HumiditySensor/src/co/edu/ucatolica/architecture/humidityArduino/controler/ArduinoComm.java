b54c620b97e9d77fa25da4231a4effb6289a13ac
JJimenez94 <JJimenez.9406@gmail.com>
2017-08-22 06:58:59 -0500

    Add files via upload

---
+++ HumiditySensor/src/co/edu/ucatolica/architecture/humidityArduino/controler/ArduinoComm.java
@@ -0,0 +1,22 @@
+package co.edu.ucatolica.architecture.humidityArduino.controler;
+
+import gnu.io.*;
+import java.io.*;
+import java.util.*;
+
+public class ArduinoComm {
+	//static Enumeration portList;
+	//static CommPortIdentifier portId;
+	//static String messageString = "color FF00FFEND";
+	//static SerialPort serialPort;
+	//static OutputStream outputStream;
+	
+	//portList = CommPortIdentifier.getPortIdentifiers();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+
+	}
+
+}
---
+++ HumiditySensor/src/co/edu/ucatolica/architecture/humidityArduino/model/City.java
@@ -0,0 +1,68 @@
+package co.edu.ucatolica.architecture.humidityArduino.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the city database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="City.findAll", query="SELECT c FROM City c")
+public class City implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="city_id")
+	private Integer cityId;
+
+	private String name;
+
+	//bi-directional many-to-one association to Capturedhumidity
+	@OneToMany(mappedBy="city")
+	private List<Capturedhumidity> capturedhumidities;
+
+	public City() {
+	}
+
+	public Integer getCityId() {
+		return this.cityId;
+	}
+
+	public void setCityId(Integer cityId) {
+		this.cityId = cityId;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Capturedhumidity> getCapturedhumidities() {
+		return this.capturedhumidities;
+	}
+
+	public void setCapturedhumidities(List<Capturedhumidity> capturedhumidities) {
+		this.capturedhumidities = capturedhumidities;
+	}
+
+	public Capturedhumidity addCapturedhumidity(Capturedhumidity capturedhumidity) {
+		getCapturedhumidities().add(capturedhumidity);
+		capturedhumidity.setCity(this);
+
+		return capturedhumidity;
+	}
+
+	public Capturedhumidity removeCapturedhumidity(Capturedhumidity capturedhumidity) {
+		getCapturedhumidities().remove(capturedhumidity);
+		capturedhumidity.setCity(null);
+
+		return capturedhumidity;
+	}
+
+}
